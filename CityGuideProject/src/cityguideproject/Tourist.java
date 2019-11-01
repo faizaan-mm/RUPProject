4eca9b0afabe6de8a9423aeee91ee9b8c0593bd3
faizaan-mm <faizaan.mm@gmail.com>
2019-11-01 23:52:14 +0530

    added Tourist map

--- CityGuideProject/src/cityguideproject/Tourist.java
+++ CityGuideProject/src/cityguideproject/Tourist.java
@@ -4,7 +4,15 @@
  * and open the template in the editor.
  */
 package cityguideproject;
+import static com.teamdev.jxbrowser.engine.RenderingMode.HARDWARE_ACCELERATED;
 
+import com.teamdev.jxbrowser.browser.Browser;
+import com.teamdev.jxbrowser.engine.Engine;
+import com.teamdev.jxbrowser.engine.EngineOptions;
+import com.teamdev.jxbrowser.view.swing.BrowserView;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 /**
  *
  * @author felixfaker
@@ -43,6 +51,11 @@ public class Tourist extends javax.swing.JFrame {
         jLabel1.setText("Tourist Home");
 
         jButton1.setText("Map");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Get Surroundings Info");
 
@@ -118,6 +131,11 @@ public class Tourist extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        TouristMap.open_window();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
